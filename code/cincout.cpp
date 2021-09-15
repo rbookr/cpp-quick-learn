3a19bb372904e36bd0e69dde8f14525db2b97683
rainboy <rainboylvx@qq.com>
2021-09-15 18:32:05 +0800

    update

--- code/cincout.cpp
+++ code/cincout.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+using namespace std;
 int main(){
     int a; //定义一个整型变量a
-    std::cin >> a; // 输入一个数字
-    // 把这个数字输出
-    std::cout << a << std::endl;
+    double b,c;
+    int x,y,z;
+    cin >> a >> b >>c; //都写一行太长了，所以换一行写
+    cin >> x >> y >> z;
+    //每个数占一行
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << x << endl;
+    cout << y << endl;
+    cout << z << endl;
     return 0;
 }
